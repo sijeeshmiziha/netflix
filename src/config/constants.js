a86248e147006e7c12e2fce65fef33c22e89ead3
sijeeshmiziha <sijeeshmonbalan@gmail.com>
2021-05-21 14:42:36 +0530

    Define TMDB API key and image URL constants

---
+++ src/config/constants.js
@@ -0,0 +1,4 @@
+export const baseUrl = 'https://api.themoviedb.org/3/';
+export const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
+export const imageUrl = 'https://image.tmdb.org/t/p/original';
+export const imageUrlW500 = 'https://image.tmdb.org/t/p/w500';
