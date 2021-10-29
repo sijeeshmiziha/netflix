ead0ac4b867e23d604ff2af1016f7f91bfc41f9e
N-Musa <n.musa@gmx.net>
2021-10-30 00:04:55 +0200

    Stored MovieDB API Key in environment variable

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
 export const baseUrl="https://api.themoviedb.org/3/"
-export const API_KEY="d8d00c93dca39e30c0c93f7b781c042e"
+export const API_KEY= process.env.REACT_APP_API_KEY
 export const imageUrl="https://image.tmdb.org/t/p/original" 
